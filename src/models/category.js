a5223a54dd9c053e3cfac0c3a6de15de8632c5c7
jackcorless91 <146907668+jackcorless91@users.noreply.github.com>
2025-03-10 17:41:34 +1100

    create category model

---
+++ src/models/category.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose")
+
+const CategorySchema = mongoose.Schema({
+    title: String,
+    description: String,
+}, {
+    collection: "categories"
+})
+
+const Category = mongoose.model("Category", CategorySchema)
